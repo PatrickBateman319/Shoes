d47a88cff6db25b1472fabbc85f3c1ebd996964a
PatrickBateman319 <141443937+PatrickBateman319@users.noreply.github.com>
2023-08-05 20:41:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-let currentAmount = 0;
-const maxAmount = 500;
+let currentProgress = 0;
 
-function updateProgressBar() {
-    const progressFill = document.getElementById('progressFill');
-    progressFill.textContent = `${currentAmount.toFixed(2)}/${maxAmount}$`;
-    const progressBarWidth = (currentAmount / maxAmount) * 100;
-    progressFill.style.width = `${progressBarWidth}%`;
-
-    if (currentAmount >= maxAmount) {
-        const donateButton = document.getElementById('donateButton');
-        donateButton.disabled = true;
-        progressFill.textContent = 'დიდი მადლობა გაწეული სამსახურისთვის, უფალი შეგეწიოთ';
+function updateProgress() {
+    currentProgress += 0.99;
+    if (currentProgress >= 500) {
+        document.getElementById("progress").innerText = "500/500$";
+        document.getElementById("message").innerText = "დიდი მადლობა გაწეული სამსახურისთვის, უფალი შეგეწიოთ";
+        document.getElementById("donateButton").disabled = true;
+    } else {
+        document.getElementById("progress").innerText = currentProgress.toFixed(2) + "/500$";
     }
 }
-
-document.getElementById('donateButton').addEventListener('click', () => {
-    if (currentAmount < maxAmount) {
-        currentAmount += 0.99;
-        updateProgressBar();
-    }
-});
-
